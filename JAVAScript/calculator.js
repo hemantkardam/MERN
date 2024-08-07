29b6c3c2bb287c6d74c0f89837630b458f0f85f5
hemantkardam <hemantkardam8287@gmail.com>
2024-08-07 21:33:22 +0530

    Update

---
+++ JAVAScript/calculator.js
@@ -0,0 +1,16 @@
+const prompt =require('prompt-sync')()
+
+//Parsefloat for convert string to float.
+//ParseInt for convert string to Int.
+const num1=parseFloat(prompt('Enter a num1 : '));
+const num2=parseFloat(prompt('Enter a num2 : '));
+const Operator=prompt('Enter a Operator : ');
+
+// switch(Operator){
+//     case '+':console.log(`num1+" " +Operator+" " num2 +" " + "="+"  "`);console.log(num1+num2);break;;
+//     case '-':console.log(num1-num2);break;
+//     case '*':console.log(num1*num2);break;        
+//     case '/':console.log(num1/num2);break;
+//     default:console.log("Invalid choice.");break;
+// }
+console.log(`$(num1) $(Operator) $(num2)` );
