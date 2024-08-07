e956eaf2e428c1af2a6e6fcd028415b9a5953aad
hemantkardam <hemantkardam8287@gmail.com>
2024-08-07 21:33:45 +0530

    Update

---
+++ JAVAScript/if.js
@@ -0,0 +1,12 @@
+const prompt = require('prompt-sync')()
+let a=prompt('Enter your age : ');
+
+if(a>=18){
+    console.log("You are eligible for voting.");
+}
+else if(a<=0){
+    console.log("First you born.");
+}
+else{
+    console.log("You are not eligible for voting.");
+}
