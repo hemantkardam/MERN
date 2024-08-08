1e649c778ae366849c64501774e076c0d3e7e9aa
hemantkardam <hemantkardam8287@gmail.com>
2024-08-08 12:30:24 +0530

    update

---
+++ JAVAScript/array.js
@@ -0,0 +1,38 @@
+const prompt=require('prompt-sync')()
+
+let arr = [1,2,3,4,5,6,7,8,9,10];
+console.log(arr);
+
+// For find the lenght of array.
+// console.log(arr.length);
+
+// For add value in last indexing of array use push method.
+// arr.push("Delete");
+// console.log(arr);
+
+// For remove value from the last indexing in array is use pop method.
+// arr.pop();
+// console.log(arr);
+
+// For add value in First indexing of array use push method.
+// arr.unshift("Hey");
+// console.log(arr);
+
+// For remove value from the First indexing in array is use pop method.
+// arr.shift();
+// console.log(arr);
+
+// For cut the element from array and store in other variable  use slice method.
+// create variable variable_name = array_name.slice(starting index,Laste index + 1)
+// const sliced = arr.slice(2,4);
+// console.log(sliced);
+// console.log(arr);
+
+//
+// array_name.splice(starting indexing , no of elements wants to remove , what you want to add value);
+// arr.splice(3,2,"hemant");
+// console.log(arr);
+
+// When you want only insert value in between arrays.
+// arr.splice(3,0,4,5);
+// console.log(arr);
