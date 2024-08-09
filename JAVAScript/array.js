2fed23d66c1764e3ca6d80e01aa9d5e22aaa230d
hemantkardam <hemantkardam8287@gmail.com>
2024-08-09 21:32:40 +0530

    update

--- JAVAScript/array.js
+++ JAVAScript/array.js
@@ -1,7 +1,7 @@
 const prompt=require('prompt-sync')()
 
-let arr = [1,2,3,4,5,6,7,8,9,10];
-console.log(arr);
+// let arr = [1,2,3,4,5,6,7,8,9,10];
+// console.log(arr);
 
 // For find the lenght of array.
 // console.log(arr.length);
@@ -36,3 +36,54 @@ console.log(arr);
 // When you want only insert value in between arrays.
 // arr.splice(3,0,4,5);
 // console.log(arr);
+
+// For sorting the array, use array_name.sort() method. for only when all value has single digits (0 to 9) or only when all value has double digits (11 to 99) means all value has same like single,double digits but when its mixed array not sort.
+
+    // Array sorted
+    // let arr =[2,6,3,7,2,1,4,7,8,9];
+    // arr.sort();
+    // console.log(arr);
+
+    // Array sorted
+    // let arr =[22,63,34,75,26,21,34,57,68,69];
+    // arr.sort();
+    // console.log(arr);
+
+    // Array not sorted
+    // let arr =[222,63,4,75,26,21,334,57,8,69];
+    // arr.sort();
+    // console.log(arr);
+    
+// For join arrays like in onle line and join with symbol use join method.
+
+    // let arr =[222,63,4,75,26,21,334,57,8,69];
+    // const newarr = arr.join('-');
+    // console.log(newarr);
+    // console.log(arr);
+    // Answer :- 222-63-4-75-26-21-334-57-8-69.
+
+// 2-D array 
+
+    // let a = [[1,2],[2,4]];
+    // console.log(a);
+
+// Call Back function - when you used function inside the function like function(another_function).
+        // array.forEach(() => {
+        //    //code 
+        // });
+
+// foreach method - used for iterate each value of array.
+// syntax - variable_name.foreach( ("value_parameters","indexing_parameters")=>{ code })
+    // let arr= [1,2,3,4,5];
+    // arr.forEach((value)=>{
+    //         console.log(value);
+    // })
+    
+// Map method - used for iterate each value of array.
+// syntax - variable_name.map( ("value_parameters","indexing_parameters")=>{ code })
+    // let arr= [1,2,3,4,5,6];
+    // const a = arr.map((value)=>{
+    //     return value;
+    // })
+    // console.log(a);
+
