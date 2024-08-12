c21ec97c8af8196b43446bf99d381130b3296693
hemantkardam <hemantkardam8287@gmail.com>
2024-08-12 12:33:04 +0530

    Update

---
+++ JAVAScript/date.js
@@ -0,0 +1,42 @@
+// js Date
+// New Date 
+// All date methods
+//   1.getMethods
+//      .getDate()
+//      .getTime()
+//      .getMonth()
+//
+//   2.setMethods
+//      .setDate()
+//      .setTime()
+//      .setMonth()
+
+
+// const date = new Date();
+// console.log(typeof(date));
+// console.log(date.toDateString());
+// console.log(date.toTimeString());
+// console.log(date.toISOString());
+// console.log(date.toUTCString());
+// console.log(date.toLocaleDateString());
+
+// console.log(date.getDate());
+// console.log(date.getMonth());
+// console.log(date.getFullYear());
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+// console.log(date.getSeconds());
+
+// console.log(date.getTime());
+//  date.setDate(21);
+//  date.setMonth(8);
+//  date.setFullYear(2003);
+// console.log(date);
+
+// For print infinte time
+    // const date = new Date();
+    // let n=1;
+    // while(n>=1){
+    //     console.log(date.toDateString());
+    //     n++;
+    // }
