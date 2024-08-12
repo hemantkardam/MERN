8a36c6425ede0f9d61d90eeebb25917f4a0fbe3b
hemantkardam <hemantkardam8287@gmail.com>
2024-08-12 12:33:28 +0530

    Update

---
+++ JAVAScript/number.js
@@ -0,0 +1,25 @@
+
+// is Not a number - syntax: isNAN(_variable_name) - Chechking for variable stored a number  value or not. 
+// let num =23;
+// console.log(num);
+// console.log(isNaN(num));
+
+// variable_name.toString() - to convert number into a string datatype. 
+// const newnum = num.toString();
+// console.log(newnum);
+// console.log(typeof(newnum));
+
+// BigInt - 
+// let num =BigInt(234354656753254523);
+// console.log(num);
+
+// toFixed - for a digits print after a decimal value.
+// let num =345.66843233;
+// let newnum = num.toFixed(2);
+// console.log(newnum);
+
+// toPrecision - for a print total no of digits.
+// let num =345.66843233;
+// let newnum = num.toPrecision(3);
+// console.log(newnum);
+
