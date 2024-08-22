93f49f28e814ffdcc51247dd41ffd628c4b47bbb
Hemant Kardam <hemantkardam8287@gmail.com>
2024-08-22 16:41:47 +0530

    update

--- JAVAScript/calculator.js
+++ JAVAScript/calculator.js
@@ -6,6 +6,7 @@ const num1=parseFloat(prompt('Enter a num1 : '));
 const num2=parseFloat(prompt('Enter a num2 : '));
 const Operator=prompt('Enter a Operator : ');
 
+
 // switch(Operator){
 //     case '+':console.log(`num1+" " +Operator+" " num2 +" " + "="+"  "`);console.log(num1+num2);break;;
 //     case '-':console.log(num1-num2);break;
