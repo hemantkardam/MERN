a4f09a401987c5f2adbac5e8af2f4037a888a74b
Hemant Kardam <hemantkardam8287@gmail.com>
2024-08-27 13:24:17 +0530

    update

--- JAVAScript/dom.js
+++ JAVAScript/dom.js
@@ -1,6 +1,37 @@
 let nam ='Harry';
 let name2 = 'Hemant';
 
+// let nam ='Harry';
+    // let name2 = 'Hemant';
+
+    // document.getElementsByTagName('h1')[1].innerHTML = nam;
+    // document.getElementsByTagName('h1')[2].innerHTML = name2;
+    // document.querySelector('h1').innerHTML = name2;
+    // document.querySelector('.h1').innerHTML = name2;
+    // document.querySelector('#h1').innerHTML = name2;
+    // document.querySelectorAll('h1 ')[0].innerHTML = nam;
+    // let names = document.querySelectorAll('h1 ')[3];
+    // names.innerHTML = nam;
+    // names.style.color = 'red';
+
+    // const head = document.createElement('h1');
+    // console.log(head);
+    // const txt = document.createTextNode("Hello duniya")
+    // console.log(txt)
+    // head.appendChild(txt);
+    // document.body.appendChild(head); 
+
+    // const img = document.createElement('img');
+    // console.log(img);
+    // const alt = document.createAttribute('alt');
+    // console.log(alt);
+    // alt.value='404 not found';
+    // img.setAttribute(alt);
+    
+    document.addEventListener('click',function run(){
+        document.getElementsByTagName('h1').style.backgroundColor='Black';
+    })
+    
 document.getElementsByTagName('h1')[0].innerHTML = nam;
 
 function img(){
