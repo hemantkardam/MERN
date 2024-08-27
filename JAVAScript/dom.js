3ae8a91651e256d2a520e8b9c20fe0821dd03be6
Hemant Kardam <hemantkardam8287@gmail.com>
2024-08-27 13:22:53 +0530

    update

---
+++ JAVAScript/dom.js
@@ -0,0 +1,9 @@
+let nam ='Harry';
+let name2 = 'Hemant';
+
+document.getElementsByTagName('h1')[0].innerHTML = nam;
+
+function img(){
+    document.getElementsByTagName('img')[0].style.width='500px';
+    document.getElementsByTagName('img')[0].style.height='3 00px';
+}
