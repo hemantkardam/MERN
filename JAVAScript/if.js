6a7b327a746d6206ebc0b0d319fdbf2e0a6a6c71
Hemant Kardam <hemantkardam8287@gmail.com>
2024-08-30 23:49:08 +0530

    update

--- JAVAScript/if.js
+++ JAVAScript/if.js
@@ -1,13 +1,16 @@
-const prompt = require('prompt-sync')()
-let a=prompt('Enter your age : ');
+// const prompt = require('prompt-sync')()
+// let a=prompt('Enter your age : ');
 
-if(a>=18){
-    console.log("You are eligible for voting.");
-}
-else if(a<=0){
-    console.log("First you born.");
-}
-else{
-    console.log("You are not eligible for voting.");
-}
+// if(a>=18){
+//     console.log("You are eligible for voting.");
+// }
+// else if(a<=0){
+//     console.log("First you born.");
+// }
+// else{
+//     console.log("You are not eligible for voting.");
+// }
 
+
+let a = prompt("Enter a no");
+console.log(a);
