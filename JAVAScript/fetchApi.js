ceb8c1fbf6255f027500417115bc1d228818c1a1
Hemant Kardam <hemantkardam8287@gmail.com>
2024-09-06 18:58:59 +0530

    update

--- JAVAScript/fetchApi.js
+++ JAVAScript/fetchApi.js
@@ -1,10 +1,17 @@
-fetch('https://dummyjson.com/products').then((res)=>{
+fetch("https://dummyjson.com/products")
+  .then((res) => {
     // console.log(prod);
-    return res.json()
-}).then((prod)=>{
+    return res.json();
+  })
+  .then((prod) => {
     console.log(prod);
-})
-.catch((error)=>{
+    const add = prod.products;
+    let rows = "";
+    add.forEach((product) => {
+      rows += `<tr><td>${product.id}</td><td>${product.title}</td><td>${product.description}</td><td>${product.category}</td><td>${product.price}</td><td>${product.stock}</td><td>${product.rating}</td><td> <img src="${product.images}" style = "width:25% "/></td></tr>`;
+    });
+    document.getElementById("tableRows").innerHTML = rows;
+  })
+  .catch((error) => {
     console.log(error);
-})
-    
+  });
