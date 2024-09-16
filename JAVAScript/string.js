1fcc169a98f556075cdf98b3e45e123328eeb889
Hemant Kardam <hemantkardam8287@gmail.com>
2024-09-16 22:35:38 +0530

    update

---
+++ JAVAScript/string.js
@@ -0,0 +1,24 @@
+const name ="harry";
+const score =50;
+// console.log(name +score+"value");
+
+// new method to print variable. String interpolation
+// console.log(`Hello my name is ${name} and my score is ${score}.`);
+
+// intialize of string
+
+const gameName = new String('harry');
+
+console.log(gameName);
+// console.log(gameName.__proto__); //give a empty object,but its stored method of string in browser.
+// console.log(gameName.length); //give length of the string.
+// console.log(gameName.toUpperCase()); //using for a to convert into a capital letter.
+// console.log(gameName.indexOf('r')); //to find the index of character.
+// console.log(gameName.charAt(3));    //to find character bye indexing.
+console.log(gameName.charCodeAt('4'));
+// console.log(gameName)
+// console.log(gameName)
+// console.log(gameName)
+// console.log(gameName)
+// console.log(gameName)
+// console.log(gameName)
