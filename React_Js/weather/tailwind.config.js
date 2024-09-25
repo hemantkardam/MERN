c5a84bfca909620fcaa7af1bb7931179a76b98bf
Hemant Kardam <hemantkardam8287@gmail.com>
2024-09-25 13:48:04 +0530

    update

---
+++ React_Js/weather/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage:{
+        'mainImg':"url('./media/MicrosoftTeams-image.png')",
+        'leftImg':"url('./media/MicrosoftTeams-image (3) (1).png')"      
+      }
+    },
+  },
+  plugins: [],
+}
+
