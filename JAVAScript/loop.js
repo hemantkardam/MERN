b00452203a654ba45b4eb6cfc056715213aa562d
Hemant Kardam <hemantkardam8287@gmail.com>
2024-10-14 12:53:27 +0530

    update

--- JAVAScript/loop.js
+++ JAVAScript/loop.js
@@ -4,8 +4,6 @@ const prompt = require('prompt-sync')()
     // for(let i=1;i<=5;i+=1){
     //     console.log(i,".","Harry");
     // }
-
-    
     const n =prompt('Enter a value for print table : ');
 
     for(let i=5 ;i<=50;i+=5){
